bb1b3b0bbac2f875534d548c7f990fa1d402d96e
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-03-24 21:34:37 +0600

    Assginment 02 24/03/2022

---
+++ Assginment 02/index.php
@@ -0,0 +1,31 @@
+
+<?php 
+
+
+// I have three old hp monitors, not new. I want to sell it on my own website.
+//  That’s why I should create a product upload form on my website, 
+//  then upload my product with specification and available quantity.
+//   My monitors have a 15.6 inch display. My monitor’s VGA cable status,
+//    yes, I will sell it for 8000 tk.
+
+// For creating a product upload form what specifications will be uploaded and what will be their data types?
+
+
+
+ $monitor_pice = 3;             // Data type = "integer"
+ $name = "HP Monitor";            // Data Type = "String" 
+ $monitor_status_new = false;   // Data Type = "boolean"
+ $monitor_size = 15.6;          // Data Type = "duble/float"
+ $cable_status_vga = true;      // Data Type = "boolean"   
+ $amount = 8000;                // Date Type = "integer"
+
+
+
+ echo gettype($amount);
+
+
+
+
+
+
+?>
